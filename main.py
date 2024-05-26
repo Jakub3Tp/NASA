8a2bb50e1a41e892d9f0321835a2f5acb288d450
Jakub <jakub.gola2@teb.edu.pl>
2024-05-26 15:16:32 +0200

    Commit 26.05.2024 - Final

--- main.py
+++ main.py
@@ -5,19 +5,10 @@ if __name__ == '__main__':
 
     url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+    response = requests.get(url)
+    data = response.json()
 
-        data = response.json()
-
-        print("Informacje o zdjęciu kosmicznym:")
-        print(f"Tytuł: {data['title']}")
-        print(f"Data: {data['date']}")
-        print(f"Opis: {data['explanation']}")
-        print(f"URL zdjęcia: {data['url']}")
-    except requests.exceptions.HTTPError as http_err:
-        print(f"Błąd HTTP: {http_err}")
-    except Exception as err:
-        print(f"Nieoczekiwany błąd: {err}")
-
+    print("Informacje o zdjęciu kosmicznym:")
+    print(f"Tytuł: {data['title']}, Data: {data['date']}")
+    print(f"Opis: {data['explanation']}")
+    print(f"URL zdjęcia: {data['url']}")
